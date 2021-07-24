f3a1f4b806496afdd284f0726c98ef20b56ca53f
kayofujii <kayosamu1025@gmail.com>
2021-07-24 10:47:05 +0900

    検索追加

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,26 @@
         </a>
       </div>
 
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
+        <dir class="navbar-start">
+          <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has_addons">
+                <div class="control">
+                  <input type="text" class="input" placeholder="What are you looking for?" name="query">
+                </div>
+
+                <div class="control">
+                  <button class="button is-success">
+                    <span class="icon">
+                      <i class="fas fa-search"></i>
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </dir>
         <div class="navbar-end">
           <router-link to="/summer" class="navbar-item">Summer</router-link>
           <router-link to="/winter" class="navbar-item">Winter</router-link>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue';
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
 
 
 const routes = [
@@ -20,6 +21,11 @@ const routes = [
         path: '/:category_slug/',
         name: 'Category',
         component: Category
+    },
+    {
+        path: '/search',
+        name: 'Search',
+        component: Search
     },
 ];
 
