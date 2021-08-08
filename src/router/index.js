e6e02cb37de9878c11f05b963741f7037762dc93
kayofujii <kayosamu1025@gmail.com>
2021-08-08 21:03:44 +0900

    マイページの作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import store from '../store';
+
 import Home from '../views/Home.vue';
+
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
+import MyAccount from '../views/MyAccount.vue'
 
 
 const routes = [
@@ -45,11 +49,27 @@ const routes = [
         name: 'LogIn',
         component: LogIn
     },
+    {
+        path: '/my-account',
+        name: 'MyAccount',
+        component: MyAccount,
+        meta: {
+            requireLogin: true
+        }
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+        next({ name: 'Login', query: { to: to.path } });
+    } else {
+        next()
+    }
 })
 
 export default router;
