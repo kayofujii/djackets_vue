822de400c1eb087ca083e33af3e021f89ea5170f
kayofujii <kayosamu1025@gmail.com>
2021-08-09 16:41:59 +0900

    チェックアウトページの追加

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
 import MyAccount from '../views/MyAccount.vue'
+import Checkout from '../views/Checkout.vue'
 
 
 const routes = [
@@ -53,6 +54,14 @@ const routes = [
         path: '/my-account',
         name: 'MyAccount',
         component: MyAccount,
+        meta: {
+            requireLogin: true
+        }
+    },
+    {
+        path: '/cart/checkout',
+        name: 'Checkout',
+        component: Checkout,
         meta: {
             requireLogin: true
         }
